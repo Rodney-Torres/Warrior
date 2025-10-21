3ed4c216d38253d3be0c1f8c31d29cddf146461e
Rodney Torres <altodor.tv@gmail.com>
2025-10-20 22:31:51 -0400

    Lectures 83 to 87 finished
    Lecture 83 - BP Death Interface - Handle death. Lecture 84 - Material Dissolve FX - Animate scalar param. Lecture 85 - Dissolve Niagara FX - Set Niagara variable. Lecture 86 - Share Task - Share Your Progress. Lecture 87 - Pawn UI Component - UI notify system.

--- Source/Warrior/Private/Characters/WarriorHeroCharacter.cpp
+++ Source/Warrior/Private/Characters/WarriorHeroCharacter.cpp
@@ -13,6 +13,7 @@
 #include "AbilitySystem/WarriorAbilitySystemComponent.h"
 #include "DataAssets/StartUpData/DataAsset_HeroStartUpData.h"
 #include "Components/Combat/HeroCombatComponent.h"
+#include "Components/UI/HeroUIComponent.h"
 
 #include "WarriorDebugHelper.h"
 
@@ -41,11 +42,17 @@ AWarriorHeroCharacter::AWarriorHeroCharacter()
 
 	HeroCombatComponent = CreateDefaultSubobject<UHeroCombatComponent>(TEXT("HeroCombatComponent"));
 
+	HeroUIComponent = CreateDefaultSubobject<UHeroUIComponent>(TEXT("HeroUIComponent"));
 }
 
 UPawnCombatComponent* AWarriorHeroCharacter::GetPawnCombatComponent() const
 {
 	return HeroCombatComponent;
+}
+
+UPawnUIComponent* AWarriorHeroCharacter::GetPawnUIComponent() const
+{
+	return HeroUIComponent;
 }
 
 void AWarriorHeroCharacter::PossessedBy(AController* NewController)
---
+++ Source/Warrior/Public/Components/UI/HeroUIComponent.h
@@ -0,0 +1,17 @@
+// Rodney Torres All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/UI/PawnUIComponent.h"
+#include "HeroUIComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UHeroUIComponent : public UPawnUIComponent
+{
+	GENERATED_BODY()
+	
+};
