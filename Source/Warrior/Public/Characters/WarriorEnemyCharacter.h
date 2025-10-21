9020b1b7cc342cf14a3a7e8e3be296093c3e40d5
RodneyTorres <rodneytorres.dev@gmail.com>
2025-10-21 15:10:59 -0400

    Lectures 88 to 95 finished
    Lecture 88 - Broadcast Value Change - Delegates. Lecture 89 - Listen For Broadcasting - Base widget class. Lecture 90 - Init Enemy Created Widget - Enemy init created widgets. Lecture 91 - Template Widgets - Create template widgets. Lecture 92 - Set Status Bar Fill Color - Change color by percent. Lecture 93 - Template Icon Slot - Slot widget for icons. Lecture 94 - Hero Overlay Widget - Construct hero overlay. Lecture 95 - Enemy Health Widget Component - Enemy health bar.

--- Source/Warrior/Public/Characters/WarriorEnemyCharacter.h
+++ Source/Warrior/Public/Characters/WarriorEnemyCharacter.h
@@ -8,6 +8,7 @@
 
 class UEnemyCombatComponent;
 class UEnemyUIComponent;
+class UWidgetComponent;
 /**
  * 
  */
@@ -25,9 +26,12 @@ public:
 
 	//~ Begin IPawnUIInterface Interface.
 	virtual UPawnUIComponent* GetPawnUIComponent() const override;
+	virtual UEnemyUIComponent* GetEnemyUIComponent() const override;
 	//~ End IPawnUIInterface Interface
 
 protected:
+	virtual void BeginPlay() override;
+
 	//~ Begin APawn Interface.
 	virtual void PossessedBy(AController* NewController) override;
 	//~ End APawn Interface
@@ -37,6 +41,10 @@ protected:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "UI")
 	UEnemyUIComponent* EnemyUIComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "UI")
+	UWidgetComponent* EnemyHealthWidgetComponent;
+
 
 private:
 	void InitEnemyStartUpData();
