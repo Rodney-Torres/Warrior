b404f8a25c7e45d3318c1e7f41aca85550d5dc49
RodneyTorres <rodneytorres.dev@gmail.com>
2025-10-28 12:39:31 -0400

    Finished lectures 121 to 128
    Lecture 121 - Notify Melee Hit Event- Send gameplay event to owning pawn. Lecture 122 - Enemy Damage Effect Spec Handle - Make effect spec handle for enemy. Lecture 123 - Apply Enemy Damage - Create new curves for damage. Lecture 124 - Motion Warping Component - Create component for all characters. Lecture 125 - Update Motion Warp Target - Update target from location. Lecture 126 - Construct Native BTTask - Create BTTask in cpp. Lecture 127 - Rotate Enemy In Task - Ticking in native task. Lecture 128 - Melee Attack Branch - New branch for attacking.

--- Source/Warrior/Private/Components/Combat/EnemyCombatComponent.cpp
+++ Source/Warrior/Private/Components/Combat/EnemyCombatComponent.cpp
@@ -2,13 +2,46 @@
 
 
 #include "Components/Combat/EnemyCombatComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "WarriorGameplayTags.h"
 
 #include "WarriorDebugHelper.h"
 
 void UEnemyCombatComponent::OnHitTargetActor(AActor* HitActor)
 {
-	if (HitActor)
+	if (OverlappedActors.Contains(HitActor))
 	{
-		Debug::Print(GetOwningPawn()->GetActorNameOrLabel() + TEXT(" is hitting ") + HitActor->GetActorNameOrLabel());
+		return;
+	}
+
+	OverlappedActors.AddUnique(HitActor);
+
+	//TODO:Implement Block Check.
+
+	bool bIsValidBlock = false;
+
+	const bool bIsPlayerBlocking = false;
+	const bool bIsMyAttackUnblockable = false;
+
+	if (bIsPlayerBlocking && !bIsMyAttackUnblockable)
+	{
+		//TODO: Check if the block is valid
+	}
+
+	FGameplayEventData EventData;
+	EventData.Instigator = GetOwningPawn();
+	EventData.Target = HitActor;
+
+	if (bIsValidBlock)
+	{
+		//TODO: Handle successful block
+	}
+	else
+	{
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+			GetOwningPawn(),
+			WarriorGameplayTags::Shared_Event_MeleeHit,
+			EventData
+		);
 	}
 }
